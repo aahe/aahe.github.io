9a4087a631cb8feefda6d3450946baf7f3d3e135
Henry Majoros <majoros.henry@gmail.com>
2015-05-18 14:23:49 -0400

    migrating from old repo

---
+++ static/js/ui.js
@@ -0,0 +1,25 @@
+/*------------------------------------------/
+ * Semi Alea User Interface Library
+ * http://semialea.com/
+ *
+ * Copyright (c) 2009 Erol Zafer Ahmed for the
+ * Union for Design.
+ * http://unionfordesign.com
+ *
+ * September 14, 2009
+ * Revision: 1
+ /-----------------------------------------*/
+ 
+
+
+/* Index Page Logo Loader */
+
+$(document).ready(function () {
+
+    $(document).ready(function () {
+        $("div#splash").css("display","none");
+    });
+
+    $("div:hidden:first").fadeIn(1500);
+    
+});
